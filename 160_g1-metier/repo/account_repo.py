aa9c7f91fa22a0579fa6a08c7a2b661110d5bc45
Findxs <colin.broutel@student.junia.com>
2025-02-05 14:06:40 +0100

    refactor: create repo files

---
+++ 160_g1-metier/repo/account_repo.py
@@ -0,0 +1,7 @@
+from DTO import *
+
+def find_by_id(id):
+    pass
+
+def get_all_accounts():
+    pass
--- 160_g1-metier/service/account_service.py
+++ 160_g1-metier/service/account_service.py
@@ -1,5 +1,5 @@
 from DTO import *
+from repo import account_repo
 
-def find_by_id():
-    pass
-
+def find_by_id(id):
+    account_repo.find_by_id(id)
