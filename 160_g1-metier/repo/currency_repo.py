977a3c040986d788eb0df79855967c2ee165c82b
Alexandre HERSSENS <alexande.herssens@student.junia.com>
2025-02-18 12:11:34 +0100

    fix mappers

--- 160_g1-metier/repo/currency_repo.py
+++ 160_g1-metier/repo/currency_repo.py
@@ -9,11 +9,11 @@ class CurrencyRepo(Repository):
     def get_currencies(self):
         try:
             self.connection.execute("SELECT * FROM currency")
-            return [self.mapper.map(row) for row in self.connection.fetchall()]
+            return [self.mapper.map_to_dto(row) for row in self.connection.fetchall()]
         except Exception as e:
             self.connection.execute("ROLLBACK")
             raise e
 
     def get_currency(self, label):
         self.connection.execute("SELECT * FROM currency WHERE code = %s", (label,))
-        return self.mapper.map(self.connection.fetchone())
+        return self.mapper.map_to_dto(self.connection.fetchone())
