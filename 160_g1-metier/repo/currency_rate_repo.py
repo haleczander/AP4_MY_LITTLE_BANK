7708cb2b3d0bf6cf1866dfb174af8721eb33a796
Findxs <colin.broutel@student.junia.com>
2025-02-19 15:44:21 +0100

    feat: implement set_currency_rate repo

--- 160_g1-metier/repo/currency_rate_repo.py
+++ 160_g1-metier/repo/currency_rate_repo.py
@@ -5,6 +5,15 @@ class CurrencyRateRepo(Repository):
     def __init__(self):
         super().__init__(CurrencyRateMapper())
 
-    def set_currency_rate(self, label):
-
-        "tbd"
+    def set_currency_rate(self, currency, target_currency, rate):
+        self.begin()
+        self.connection.execute(
+            f"""
+            INSERT INTO currency_rate (currency, target_currency, rate) 
+            VALUES ( %s, %s, %s )
+            RETURNING {self._all_fields()}
+            """,
+            (currency, target_currency, rate),
+        )
+        self.commit()
+        return self.map_to_dto( self.connection.fetchone() )
