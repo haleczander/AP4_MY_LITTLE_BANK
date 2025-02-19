d0ddea3fc6b20045a859451d51454faa2c448651
Findxs <colin.broutel@student.junia.com>
2025-02-19 15:49:35 +0100

    fix: set _all_fields() in currency_rate_repo

--- 160_g1-metier/repo/currency_rate_repo.py
+++ 160_g1-metier/repo/currency_rate_repo.py
@@ -2,8 +2,13 @@ from .repository import Repository
 from mappers import CurrencyRateMapper
 
 class CurrencyRateRepo(Repository):
+    ALL_FIELDS = ["currency", "target_currency", "rate"]
+
     def __init__(self):
         super().__init__(CurrencyRateMapper())
+
+    def _all_fields(self):
+        return ", ".join(self.ALL_FIELDS)
 
     def set_currency_rate(self, currency, target_currency, rate):
         self.begin()
